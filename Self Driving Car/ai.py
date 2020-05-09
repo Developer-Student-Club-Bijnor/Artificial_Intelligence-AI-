4b87bea826aca9da0d37c2e815863535ea9233a8
Kumar shanu <kumarshanu1009@gmail.com>
2020-05-09 16:14:04 +0530

    Adding Self Driving  Car with Deep Q Learning

---
+++ Self Driving Car/ai.py
@@ -0,0 +1,93 @@
+# AI for Self Driving Car
+
+# Importing the libraries
+import numpy as np
+import random
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.autograd as autograd
+from torch.autograd import Variable
+	
+# Creating the architechture of Neural Network
+class Network(nn.Module):
+
+	def __init__(self, input_size=5, nb_action=3):
+		super(Network, self).__init__()
+		self.input_size = input_size
+		self.nb_action = nb_action
+		self.fc1 = nn.Linear(input_size, 30)
+		self.fc2 = nn.Linear(30, nb_action)
+
+	def forward(self, state):
+		x = F.relu(self.fc1(state))
+		q_values = self.fc2(x)
+		return q_values
+
+# Implementing the Experience Replay
+class ReplayMemory(object):
+
+	def __init__(self, capacity=100):
+		self.capacity = capacity
+		self.memory = []
+
+	def push(self, event):
+		self.memory.append(event)
+		if len(self.memory) > self.capacity:
+			del self.memory[0]
+
+	def sample(self, batch_size=32):
+		samples = zip(*random.sample(self.memory, batch_size))
+		return map(lambda x: Variable(torch.cat(x,0)), samples)
+
+# Implementing the Deep Q Learning
+class Dqn():
+	"""
+		Deep Q Learning Network.
+		
+		Parameters
+		-----------------------
+		input_size: No of input features.
+
+		nb_action: No. of outputs i.e. No of actions
+		
+		gamma: value for gamma parameter.
+
+		-----------------------
+
+	"""
+
+	def __init__(self, input_size, nb_action, gamma):
+		self.gamma 			= gamma
+		self.reward_window 	= []
+		self.model 			= Network(input_size, nb_action)
+		self.memory 		= ReplayMemory(capacity=100000)
+		self.optimizer 		= optim.Adam(self.model.parameters(), lr=0.001)
+		self.last_state		= torch.Tensor(input_size).unsqueeze(0)
+		self.last_action	= 0
+		self.last_reward	= 0
+
+	def select_action(self, state):
+		probs = F.softmax(self.model(Variable(state, volatile=True))*7) # T=7 
+		action = probs.multinomial()
+		return action.data[0, 0]
+
+	def learn(self, batch_state, batch_next_state, batch_reward, batch_action):
+		outputs = self.model(batch_state).gather(1, batch_action.unsqueeze(1)).squeeze(1)
+		next_outputs = self.model(batch_next_state).detach().max(1)[0]
+		target =  self.gamma * next_outputs + batch_reward
+		td_loss = F.smooth_l1_loss(outputs, target)
+		self.optimizer.zero_grad()
+		td_loss.backward(retian_variables=True)
+		self.optimizer.step()
+		
+
+
+
+	def update(self, a, b):
+		pass
+
+	def score(self,):
+		pass
